b11ded58b7ca5ef4eac1cc4db2a89938de645afc
grant1994 <grant1994@ymail.com>
2014-11-26 01:01:37 -0800

    compareCards base
    added skeleton for the compareCards code
    todo:
    what states do what
    what is in mem64 that we are comparing

--- VGA files/Proj File/compareCards.sv
+++ VGA files/Proj File/compareCards.sv
@@ -1,9 +1,6 @@
 /* ECPE 174 - Adv. Digital Design 
-	Lab #  : 
-	Author : Kelvin Flores 
-	Date : 
-	Due :  
-	
+	Memory Card Game
+	Author : Kelvin Flores , Joseph Grant
 */
 
 /*compare 2 cards - always read the input address from the memory and compare it
@@ -14,13 +11,53 @@
   
 */
 
-module compareCards (	input logic clock,A,inputState,
-								input logic[5:0] mem6x6,
-								output logic GO);
-	//
-	//some flip flops to save values when button A is pressed
+module compareCards(
+	input logic clock,A,inputState,
+	input logic[5:0] mem6x6,
+	output logic GO
+);
+	mem64 mr64(.clock(clock),.rAddr(rAddr),.dataOut(dataOut));	
+	reg[5:0] cardmem1;
+	reg[4:0] data1;
+	reg[5:0] cardmem2;
+	reg[4:0] data2;
+	integer pairsFound=0; // This int is used to keep track of cards we have compared
 	
-	mem64 mr64(.clock(clock),.rAddr(rAddr),.dataOut(dataOut));
+	//We want the game to play, initialize GO=0 to GO play the game!
+	initial
+	begin
+		GO=0;
+	end
+	
+	//mem6x6 is assigned to read address given to memory, we want to read from there
+	assign rAddr = mem6x6;
+	
+	//flip flop assigning card values (data1/data2)/memory locations(cardmem1/cardmem2)
+	always_ff@(posedge clock)
+	begin
+		//if our state is 1, we will know we are choosing the first card
+		if(A && inputState==XX)
+			cardmem1<=mem6x6;
+			data1<=dataOut;	
+		//if our state is 2, we will know we are choosing the second card
+		if(A && inputState==XX)
+			cardmem2<=mem6x6;
+			data2=dataOut;
+	end
+	
+	//use combinational logic for data1/data2 for comparisons
+	always_comb@(data1,data2)
+	begin
+		if(data1==data2)
+		begin
+			pairsFounds<=pairsFound+1;
+		end
+	end
+	
+	//comb logic for seeing if game is over
+	always_comb@(pairsFound)
+	if(pairsFound==18)
+		GO=1;
 	
 endmodule
 						  
